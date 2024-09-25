f302f4de7eb1e7aeba12b565cce16beeaa4051c5
GABRIEL AGUIAR <153838668+GabrielDEVEL0PER@users.noreply.github.com>
2024-09-25 18:22:01 +0000

    Host corrigido para replit

--- Trabalho cateq/servidor.py
+++ Trabalho cateq/servidor.py
@@ -12,4 +12,4 @@ def confirmacao_pedido():
     return render_template('confirmacao.html', mesa_id=mesa_id)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
